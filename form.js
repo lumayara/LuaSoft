6150dd9f3f420a72bb373c577522f8d454bde392
Lucas D'Alessandro <lucasfdale@gmail.com>
2023-05-03 17:10:28 -0700

    Nav bar color change js, logo color, responsiviness fixes
    Logo: color change
    Navbar: new js that changes color when user scrolls the page
    All sections: responsiviness fixes
    Form: code updated

--- form.js
+++ form.js
@@ -1,25 +1,26 @@
-function validateForm() {
-    var nameInput = document.getElementById("name");
-    var emailInput = document.getElementById("email");
-    var companyInput = document.getElementById("company");
-    var messageInput = document.getElementById("message");
-    var submitBtn = document.getElementById("submit-btn");
-    
-    if (nameInput.value.trim() == "" || emailInput.value.trim() == "" || companyInput.value.trim() == "" || messageInput.value.trim() == "") {
+const validateForm = () => {
+    const nameInput = document.getElementById("name");
+    const emailInput = document.getElementById("email");
+    const companyInput = document.getElementById("company");
+    const messageInput = document.getElementById("message");
+    const submitBtn = document.getElementById("submit-btn");
+    if (nameInput.value.trim() === "" || emailInput.value.trim() === "" || companyInput.value.trim() === "" || messageInput.value.trim() === "") {
       submitBtn.disabled = true;
-	  submitBtn.hover = false;
+	    submitBtn.hover = false;
       return false;
     }
     
     submitBtn.disabled = false;
-	submitBtn.hover = true;
+	  submitBtn.hover = true;
     return true;
   }
 
-var nameInput = document.getElementById("name");
-var emailInput = document.getElementById("email");
-var messageInput = document.getElementById("message");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const companyInput = document.getElementById("company");
+const messageInput = document.getElementById("message");
 
 nameInput.addEventListener("input", validateForm);
 emailInput.addEventListener("input", validateForm);
+companyInput.addEventListener("input", validateForm);
 messageInput.addEventListener("input", validateForm);
