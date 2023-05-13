13d0c881c1141b2d2a2e99e3e8a8c7868118a0b4
Lucas D'Alessandro <lucasfdale@gmail.com>
2023-05-12 21:14:42 -0700

    more animations

--- nav-bar.js
+++ nav-bar.js
@@ -4,15 +4,15 @@ const worksSection = document.querySelector('#works');
 const aboutSection = document.querySelector('#about');
 const contactSection = document.querySelector('#contact');
 
-const heroOptions = {
-	rootMargin: "75px 0px 0px 0px",
-};
+// const heroOptions = {
+// 	rootMargin: "75px 0px 0px 0px",
+// };
 
-const viewPortHeightOffSet = document.querySelector("#hero").offsetHeight > 600 ? 
-document.querySelector("#hero").offsetHeight : 600;
-const aboutOptions = {
-	rootMargin: `0px 0px -${viewPortHeightOffSet}px 0px`,
-};
+// const viewPortHeightOffSet = document.querySelector("#hero").offsetHeight > 600 ? 
+// document.querySelector("#hero").offsetHeight : 600;
+// const aboutOptions = {
+// 	rootMargin: `0px 0px -${viewPortHeightOffSet}px 0px`,
+// };
 
 
 const intervals = [0,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1];
