61cee8cfce1ecd9ea31ae045a6d72fd87a892b60
Lucas D'Alessandro <lucasfdale@gmail.com>
2023-05-12 17:41:21 -0700

    Some animations

--- nav-bar.js
+++ nav-bar.js
@@ -5,40 +5,69 @@ const aboutSection = document.querySelector('#about');
 const contactSection = document.querySelector('#contact');
 
 const heroOptions = {
-  rootMargin: "75px 0px 0px 0px",
+	rootMargin: "75px 0px 0px 0px",
 };
 
 const viewPortHeightOffSet = document.querySelector("#hero").offsetHeight > 600 ? 
-                                document.querySelector("#hero").offsetHeight : 600;
+document.querySelector("#hero").offsetHeight : 600;
 const aboutOptions = {
-    rootMargin: `0px 0px -${viewPortHeightOffSet}px 0px`,
+	rootMargin: `0px 0px -${viewPortHeightOffSet}px 0px`,
 };
 
 
-const heroSectionObserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (!entry.isIntersecting) {
-        header.classList.add('nav-scrolled-black');
-      } else {
-        header.classList.remove('nav-scrolled-black');
-      }
-    });
-  }, heroOptions);
-  
-const aboutSectionObserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        // console.log(entry)
-        header.classList.add('nav-scrolled-white');
-        header.classList.remove('nav-scrolled-black');
-      } else {
-        header.classList.add('nav-scrolled-black');
-        header.classList.remove('nav-scrolled-white');
-      }
-    });
-}, aboutOptions);
+const intervals = [0,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1];
+
+const worksSectionObserver = new IntersectionObserver(entries => {
+	entries.forEach(entry=>{
+		console.log(entry.intersectionRatio)
+
+		if(entry.intersectionRatio>0.5 && entry.boundingClientRect.y>0){
+			worksSection.firstElementChild.classList.add('appear')
+		}
+		if(entry.boundingClientRect.y<0){
+			header.classList.add('nav-scrolled-white');
+		} else {
+			header.classList.remove('nav-scrolled-white');
+		}
+	})
+}, {
+	threshold: intervals
+})
+
+// setTimeout(() => {worksSectionObserver.observe(worksSection)},
+// 2000)
+worksSectionObserver.observe(worksSection)
 
 
-heroSectionObserver.observe(heroSection);
-aboutSectionObserver.observe(aboutSection);
-  
+
+
+
+// const heroSectionObserver = new IntersectionObserver(entries => {
+// 	entries.forEach(entry => {
+// 		if (!entry.isIntersecting) {
+// 			header.classList.add('nav-scrolled-black');
+// 		} else {
+// 			header.classList.remove('nav-scrolled-black');
+// 		}
+// 	});
+// }, heroOptions);
+
+// const aboutSectionObserver = new IntersectionObserver(entries => {
+// 	entries.forEach(entry => {
+// 		if (entry.isIntersecting) {
+// 			header.classList.add('nav-scrolled-white');
+// 			header.classList.remove('nav-scrolled-black');
+			
+// 		} else {
+// 			if(header.getBoundingClientRect().bottom > entry.boundingClientRect.bottom){
+// 				header.classList.add('nav-scrolled-black');
+// 				header.classList.remove('nav-scrolled-white');
+// 			}
+			
+// 		}
+// 	});
+// }, aboutOptions);
+
+
+// heroSectionObserver.observe(heroSection);
+// aboutSectionObserver.observe(aboutSection);
