8438b499a9d85b0276565f142a7dd466800891ae
Luana Mayara Fernandes da Silva Bezerra <lumayara.ads@gmail.com>
2024-02-13 23:17:11 -0500

    Revert "Merge pull request #16 from lumayara/LuisC"
    This reverts commit 72e38bc1c29467c3bd86ba2fa335765de5662513, reversing
    changes made to f049679f4a16a54aa43dc4785874931b3bd49dbc.

--- nav-bar.js
+++ nav-bar.js
@@ -48,12 +48,6 @@ hamburger.addEventListener("click", () => {
 	navMenu.classList.toggle("active");
 })
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-	hamburger.classList.remove("active");
-	navMenu.classList.remove("active");
-
-}))
-
 sections.forEach(section => {
 	observer.observe(section)
 });
